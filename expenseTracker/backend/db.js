86ee96440af2501291343c74c488a4efcbe7f973
rojanagunoori <nagunooriroja@gmail.com>
2024-10-22 23:22:44 +0530

    first commit

--- expenseTracker/backend/db.js
+++ expenseTracker/backend/db.js
@@ -1,25 +1,26 @@
-const sqlite3=require("sqlite3").verbose()
-const path=require("path")
+
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
 
+const dbPath = path.resolve(__dirname, 'expenseTracker.db');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Error opening SQLite database:', err.message);
+  } else {
+    console.log('Connected to SQLite database.');
+  }
+});
 
-const dbPath=path.resolve(__dirname,'expenseTracker.db')
-const db=new sqlite3.Database(dbPath,(err)=>{
-    if(err){
-        console.error('Error opening SQLite database:', err.message);
-    }else{
-        console.log('Connected to SQLite database.');
-    }
-})
 
-db.serialize(()=>{
-    db.run(`
-        CREATE TABLE IF NOT EXISTS categories (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        type TEXT NOT NULL CHECK (type IN ('income','expense'))
-        )
-    `);
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS categories (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      type TEXT NOT NULL CHECK (type IN ('income', 'expense'))
+    )
+  `);
 
   db.run(`
     CREATE TABLE IF NOT EXISTS transactions (
@@ -32,7 +33,6 @@ db.serialize(()=>{
       FOREIGN KEY (category_id) REFERENCES categories(id)
     )
   `);
-})
+});
 
-module.exports=db;
-
+module.exports = db;
